294b1f2f42a63d78a231b2b64ce19019a439eb4a
99CLOUD-MALEI <ma.leibj@99cloud.net>
2023-02-16 15:03:07 +0800

    适配16

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 
 	"golang.org/x/sys/windows/registry"
@@ -16,7 +17,23 @@ func main() {
 	defer data_k.Close()
 	registry.DeleteKey(data_k, "Data")
 	fmt.Println(`清理 HKEY_CURRENT_USER\Software\PremiumSoft\Data 完成`)
+	fmt.Println("")
+	//删除 HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration*
+	np_k, _ := registry.OpenKey(registry.CURRENT_USER, `Software\PremiumSoft\NavicatPremium`, registry.ALL_ACCESS)
+	defer np_k.Close()
+	//遍历子项 查找名称是Registration开头的子项
+	fmt.Println(`遍历 HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium 子项...`)
+	np_sub_k, _ := np_k.ReadSubKeyNames(20)
+	re := regexp.MustCompile("^Registration")
 
+	for _, sub_k := range np_sub_k {
+		if match := re.MatchString(sub_k); match {
+			fmt.Println("清理 子项:", sub_k)
+			registry.DeleteKey(np_k, sub_k)
+		}
+	}
+
+	fmt.Println("")
 	// 删除 HKEY_CURRENT_USER\Software\Classes\CLSID 子项下 的info子项
 	cls_k, _ := registry.OpenKey(registry.CURRENT_USER, `Software\Classes\CLSID`, registry.ALL_ACCESS)
 	defer cls_k.Close()
